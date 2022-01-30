4c2d4901492fabbbc1ed32b78e96b7e4f975990b
army2010 <85797689+army2010@users.noreply.github.com>
2022-01-29 23:36:06 -0800

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,36 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+color = "black";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(350, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+color = "red";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(450, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+color = "orange";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(300, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+color = "green";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(400, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
